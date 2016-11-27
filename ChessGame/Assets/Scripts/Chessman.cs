6eb3dbeabc08609f15472be16dfa475caabeef4c
maciekmusielak <maciu9262@gmail.com>
2016-11-27 13:13:09 +0100

    Ruch i wybieranie figur

---
+++ ChessGame/Assets/Scripts/Chessman.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Chessman : MonoBehaviour {
+    /*
+        // Use this for initialization
+        void Start () {
+
+        }
+
+        // Update is called once per frame
+        void Update () {
+
+        }
+    */
+    public int CurrentX{set;get;}
+    public int CurrentY{set;get;}
+    public bool ifWhite;
+
+    public void Position(int x, int y)
+    {
+        CurrentX = x;
+        CurrentY = y;
+    }
+
+    public virtual bool Possible(int x, int y)
+    {
+        return true;
+    }
+}
