5fa96b60f453af47adb8e098d7d7c0619b169bf0
maciekmusielak <maciu9262@gmail.com>
2016-12-07 00:25:02 +0100

    Chess Game
    ruch wieży zgodnie z zasadami gry

--- Rook.cs
+++ Rook.cs
@@ -2,5 +2,95 @@
 using System.Collections;
 
 public class Rook : Chessman {
+    public override bool[,] Possible()
+    {
+        bool[,] r = new bool[8, 8];
+        Chessman c1;
+        //ruch w prawo
+        int i;
+        i = CurrentX;
+        while (true)
+        {
+            //CurrentX + 1;
+            i++;
+            if (i >= 8)
+                break;
+
+            c1 = BoardManager.Instance.Chessmans[i, CurrentY];
+            if (c1 == null)
+                r[i, CurrentY] = true;
+            else
+            {
+                if (c1.ifWhite != ifWhite)
+                    r[i, CurrentY] = true;
+                break;
+            }
+        }
+
+        //ruch w lewo
+        i = CurrentX;
+        while (true)
+        {
+            //CurrentX + 1;
+            i--;
+            if (i < 0)
+                break;
+
+            c1 = BoardManager.Instance.Chessmans[i, CurrentY];
+            if (c1 == null)
+                r[i, CurrentY] = true;
+            else
+            {
+                if (c1.ifWhite != ifWhite)
+                    r[i, CurrentY] = true;
+                break;
+            }
+        }
+
+
+        //ruch przód(góra)
+        i = CurrentY;
+        while (true)
+        {
+            //CurrentX + 1;
+            i++;
+            if (i >= 8)
+                break;
+
+            c1 = BoardManager.Instance.Chessmans[CurrentX, i];
+            if (c1 == null)
+                r[CurrentX, i] = true;
+            else
+            {
+                if (c1.ifWhite != ifWhite)
+                    r[CurrentX, i] = true;
+                break;
+            }
+        }
+
+
+        //ruch tył(dół)
+        i = CurrentY;
+        while (true)
+        {
+            //CurrentX + 1;
+            i--;
+            if (i < 0 )
+                break;
+
+            c1 = BoardManager.Instance.Chessmans[CurrentX, i];
+            if (c1 == null)
+                r[CurrentX, i] = true;
+            else
+            {
+                if (c1.ifWhite != ifWhite)
+                    r[CurrentX, i] = true;
+                break;
+            }
+        }
+
+
+        return r;
+    }
 
 }
